b11d10d2c6592b7c9f6db69698a2db7f1353d45f
Jalael <jlevine7@binghamton.edu>
2024-04-27 18:32:23 -0400

    .

---
+++ controller.py
@@ -0,0 +1,20 @@
+from card import Card
+import pygame
+
+class Controller:
+    def __init__(self):
+        """
+        docstring
+        """
+        
+    def mainloop(self):
+        while(True):
+            for event in pygame.event.get():
+                if.event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                deck = [card, card]
+                card = card.Card()
+                pygame.draw(deck)
+                
+                pygame.display.flip()
